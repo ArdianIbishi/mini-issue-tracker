72bb7140ce4dc7b998d4c091f054f1e4f57e7cda
Ardian <testibishi@gmail.com>
2026-06-23 23:28:59 +0200

    Add database seeders and demo data generation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,38 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Project;
+use App\Models\Issue;
+use App\Models\Tag;
+use App\Models\Comment;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Tag::factory(15)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Project::factory(10)
+            ->create()
+            ->each(function ($project) {
+
+                $issues = Issue::factory(5)
+                    ->for($project)
+                    ->create();
+
+                $issues->each(function ($issue) {
+
+                    $issue->tags()->attach(
+                        Tag::inRandomOrder()
+                            ->take(rand(1, 3))
+                            ->pluck('id')
+                    );
+
+                    Comment::factory(rand(2, 5))
+                        ->for($issue)
+                        ->create();
+                });
+            });
     }
 }
