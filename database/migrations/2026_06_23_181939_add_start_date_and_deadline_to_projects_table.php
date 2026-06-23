1e4243e07e542dc6aa34997ffdf385ee62e2e12d
Ardian <testibishi@gmail.com>
2026-06-23 21:28:51 +0200

    Add project start date and deadline fields

--- database/migrations/2026_06_23_181939_add_start_date_and_deadline_to_projects_table.php
+++ database/migrations/2026_06_23_181939_add_start_date_and_deadline_to_projects_table.php
@@ -6,23 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+
+            $table->date('start_date')->nullable();
+
+            $table->date('deadline')->nullable();
+
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+
+            $table->dropColumn([
+                'start_date',
+                'deadline'
+            ]);
+
         });
     }
 };
