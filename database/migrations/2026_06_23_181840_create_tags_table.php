4fff65fb66c01f40fe01a81ccb50beedd09a928a
Ardian <testibishi@gmail.com>
2026-06-23 20:59:07 +0200

    Add tag table fields

--- database/migrations/2026_06_23_181840_create_tags_table.php
+++ database/migrations/2026_06_23_181840_create_tags_table.php
@@ -12,7 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tags', function (Blueprint $table) {
+
             $table->id();
+        
+            $table->string('name')->unique();
+        
+            $table->string('color')->nullable();
+        
             $table->timestamps();
         });
     }
